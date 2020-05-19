0ed49191347bd2d172750bcebcf143fa67d8ff87
vaishali1331 <64248432+vaishali1331@users.noreply.github.com>
2020-05-20 00:22:16 +0530

    Updates

---
+++ precache-manifest.7091022e0e01b2905cf3069b0f81eff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6140980aa4473be1047ec2619b4c92",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8a09062f185d9e432d8d",
+    "url": "/robofriends/static/css/main.e732d363.chunk.css"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a09062f185d9e432d8d",
+    "url": "/robofriends/static/js/main.f480f4a5.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
